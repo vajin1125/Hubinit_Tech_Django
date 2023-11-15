1c5da2eca8d1a7b9e61aab775e9c7c4e88470998
vajin1125 <31552910+vajin1125@users.noreply.github.com>
2023-11-15 19:31:49 +0200

    update5

--- hubinit_tech/settings.py
+++ hubinit_tech/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'hubinit_tech.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.dummy'
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     # 'ENGINE': 'django.db.backends.dummy'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR/"static",
 ]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 

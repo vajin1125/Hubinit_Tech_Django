de009571f189a4ba39aed8de10110b5d093cb4d0
vajin1125 <31552910+vajin1125@users.noreply.github.com>
2023-11-15 19:38:37 +0200

    update config 1

--- hubinit_tech/settings.py
+++ hubinit_tech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nmzkg_3fmh7!8l+jqb1asj0n%h$u_$y*(9=0!i(wa+d%)tz_@2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -126,11 +126,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR/'staticfiles/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
